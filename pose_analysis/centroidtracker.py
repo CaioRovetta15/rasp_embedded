d2f8d4e4400fe987d85a0a66ef37adef25defdb5
CaioRovetta15 <caiorovetta123@usp.br>
2023-10-03 15:28:28 -0300

    commented code

--- pose_analysis/centroidtracker.py
+++ pose_analysis/centroidtracker.py
@@ -2,30 +2,33 @@ from scipy.spatial import distance as dist
 from collections import OrderedDict
 import numpy as np
 
-
 class CentroidTracker():
-    def __init__(self, maxDisappeared=10,minDistance=50):
+    def __init__(self, maxDisappeared=10, minDistance=50):
+        # Initialize the CentroidTracker class
         self.nextObjectID = 0
-        self.objects = OrderedDict()
-        self.originRects = OrderedDict()
-        self.disappeared = OrderedDict()
-
-        self.maxDisappeared = maxDisappeared
-        self.minDistance = minDistance
-
+        self.objects = OrderedDict()  # Stores object IDs and their centroids
+        self.originRects = OrderedDict()  # Stores object IDs and their original rectangles
+        self.disappeared = OrderedDict()  # Stores object IDs and the number of consecutive frames they've disappeared
+        
+        # Parameters for tracking
+        self.maxDisappeared = maxDisappeared  # Maximum consecutive frames an object can disappear before deregistration
+        self.minDistance = minDistance  # Minimum distance for associating a new centroid with an existing object
     
     def register(self, centroid, rect):
+        # Register a new object with its centroid and original rectangle
         self.originRects[self.nextObjectID] = rect
         self.objects[self.nextObjectID] = centroid
         self.disappeared[self.nextObjectID] = 0
         self.nextObjectID += 1
 
     def deregister(self, objectID):
+        # Deregister an object (remove it from tracking)
         del self.originRects[objectID]
         del self.objects[objectID]
         del self.disappeared[objectID]
 
     def get_id(self, rect):
+        # Get the ID of an object based on the closest centroid to a given rectangle
         (x, y) = rect
         cX = x
         cY = y
@@ -33,7 +36,7 @@ class CentroidTracker():
         objectIDs = list(self.objects.keys())
         objectCentroids = list(self.objects.values())
         
-        D = dist.cdist(np.array(objectCentroids), [(cX, cY)])
+        D = dist.cdist(np.array(objectCentroids), [(cX, cY)])  # Calculate distance between centroids and the given point
 
         rows = D.min(axis=1).argsort()
         cols = D.argmin(axis=1)[rows]
@@ -45,22 +48,25 @@ class CentroidTracker():
         return objectID
 
     def update(self, centroids):
-
-        if(len(centroids) == 0):
+        # Update the object tracking with new centroids
+        
+        if len(centroids) == 0:
+            # Handle the case when no centroids are detected in the current frame
             for objectID in list(self.disappeared.keys()):
                 self.disappeared[objectID] += 1
                 
-                if(self.disappeared[objectID] > self.maxDisappeared):
+                if self.disappeared[objectID] > self.maxDisappeared:
                     self.deregister(objectID)
             return self.objects, self.originRects
         
         inputCentroids = np.zeros((len(centroids), 2), dtype="int")
-        for(i,center) in enumerate(centroids):
+        for (i, center) in enumerate(centroids):
             cX = int(center[0])
             cY = int(center[1])
             inputCentroids[i] = (cX, cY)
 
-        if(len(self.objects) == 0):
+        if len(self.objects) == 0:
+            # If there are no existing tracked objects, register the new centroids as objects
             for i in range(0, len(inputCentroids)):
                 centroid = inputCentroids[i]
                 cent = centroids[i]
@@ -70,7 +76,7 @@ class CentroidTracker():
             objectIDs = list(self.objects.keys())
             objectCentroids = list(self.objects.values())
 
-            D = dist.cdist(np.array(objectCentroids), inputCentroids)
+            D = dist.cdist(np.array(objectCentroids), inputCentroids)  # Calculate distance matrix
 
             rows = D.min(axis=1).argsort()
             cols = D.argmin(axis=1)[rows]
@@ -79,7 +85,6 @@ class CentroidTracker():
             usedCols = set()
 
             for (row, col) in zip(rows, cols):
-
                 if row in usedRows or col in usedCols:
                     continue
 
@@ -95,7 +100,7 @@ class CentroidTracker():
             unusedCols = set(range(0, D.shape[1])).difference(usedCols)
 
             if D.shape[0] >= D.shape[1]:
-
+                # Handle the case when there are more existing objects than new centroids
                 for row in unusedRows:
                     objectID = objectIDs[row]
                     self.disappeared[objectID] += 1
@@ -104,11 +109,10 @@ class CentroidTracker():
                         self.deregister(objectID)
 
             else:
-
+                # Handle the case when there are more new centroids than existing objects
                 for col in unusedCols:
                     centroid = inputCentroids[col]
                     rect = centroids[col]
                     self.register(centroid, rect)
                     
-        
         return self.objects, self.originRects
--- pose_analysis/main.py
+++ pose_analysis/main.py
@@ -1,97 +1,116 @@
+# Import necessary libraries
 from ultralytics import YOLO
 import cv2
 import numpy as np
 import time
+
+# Import custom modules
 from plot import Annotator
 from solve_pnp import SolvePNP
 from min_clusters import generate_minimum_clusters
 from centroidtracker import CentroidTracker
 
+# Initialize the YOLO model for object detection
 model = YOLO("yolov8n-pose.onnx")
 
-# cap = cv2.VideoCapture('output_video.mp4')
+# Open a video capture device (0 for default camera, or specify a video file)
 cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_BUFFERSIZE, 1)
-annot = Annotator(cap.read())
-solver = SolvePNP(cap.read())
-person_ct = CentroidTracker()
-cluster_ct = CentroidTracker()
 
-# Initialize the slider position
+# Initialize instances of custom classes
+annot = Annotator(cap.read())  # Used for annotating images
+solver = SolvePNP(cap.read())    # Used for solving PnP pose estimation
+person_ct = CentroidTracker()   # Centroid tracking for people
+cluster_ct = CentroidTracker()  # Centroid tracking for clusters of people
 
-# Create a callback function for the trackbar
-talk_radius = 200 #cm
+# Initialize the radius for talking clusters
+talk_radius = 200  # in centimeters
+
 # Create a window to display the video feed
 cv2.namedWindow('output')
 
-# Create a trackbar to control the slider's position
-
+# Main loop for processing video frames
 while cap.isOpened():
     ret, img_orig = cap.read()
     if not ret:
         break
-    blank_image = np.ones((img_orig.shape[0],img_orig.shape[1],3), np.uint8)*160
+
+    # Create a blank image for additional annotations
+    blank_image = np.ones((img_orig.shape[0], img_orig.shape[1], 3), np.uint8) * 160
 
     start_time = time.time()
+
+    # Perform object detection using YOLO model
     results = model(img_orig, save_txt=False, stream=True, verbose=False, imgsz=(224, 224))
-    
+
+    # Process keypoints detected by the YOLO model
     for r in results:
         kpoints = r.keypoints.data
-    
-    detection_dict={}
+
+    detection_dict = {}
     people_on_image = []
-    for i,keyPoints in enumerate(kpoints):
-        #circle the left and right wrist
+
+    # Process keypoints and perform pose estimation for each person
+    for i, keyPoints in enumerate(kpoints):
         left_wrist = keyPoints[10]
         right_wrist = keyPoints[9]
-        rvec,tvec=solver.solve(keyPoints.numpy())
+        rvec, tvec = solver.solve(keyPoints.numpy())
+
+        # Annotate the image with keypoints and pose estimation
         img_orig = annot.kpts(img_orig, keyPoints)
 
-        detection_dict[i]={'rvec':rvec,'tvec':tvec}
-        #project the points into a virtual camera above the real camera to see the 3d points in a 2d image
+        detection_dict[i] = {'rvec': rvec, 'tvec': tvec}
         image_points = solver.project_on_roof_camera(tvec)
         people_on_image.append(image_points)
 
-    person_points=person_ct.update(people_on_image)
-    #loop through the person points ordereddict and plot the points on the image
+    # Update centroid tracking for individual people
+    person_points = person_ct.update(people_on_image)
+
+    # Annotate the image with tracked people
     for i, (objectID, centroid) in enumerate(person_points[0].items()):
-
         cv2.circle(blank_image, (int(centroid[0]), int(centroid[1])), annot.lw, (0, 0, 139), -1)
-        cv2.putText(blank_image, str(objectID), (int(centroid[0]), int(centroid[1])), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 139), 2)
-    tvec_list = [detection_dict[i]['tvec'] for i in range(len(detection_dict))]
+        cv2.putText(blank_image, str(objectID), (int(centroid[0]), int(centroid[1])),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 139), 2)
 
-    # Convert the list of arrays into a numpy array
+    # Extract and process translation vectors for clustering
+    tvec_list = [detection_dict[i]['tvec'] for i in range(len(detection_dict))]
     tvec_array = np.array(tvec_list).reshape(-1, 3)
 
-    # Generate clusters
-    clusters = generate_minimum_clusters(tvec_array, max_radius=talk_radius, min_points_per_cluster=1,plot_2d=False) #max_radius=50 (cm), min_points_per_cluster=1
-    
+    # Generate minimum clusters of people based on their positions
+    clusters = generate_minimum_clusters(tvec_array, max_radius=talk_radius, min_points_per_cluster=1, plot_2d=False)
+
+    # Annotate the image with cluster information
     for i, cluster in enumerate(clusters):
         cluster_points = np.array([tvec_list[idx] for idx in cluster])
         cluster_center = np.mean(cluster_points, axis=0)
 
-        radius_distance = np.array([0,0,talk_radius],dtype='float32').reshape(3, 1)
+        radius_distance = np.array([0, 0, talk_radius], dtype='float32').reshape(3, 1)
         cluster_radius = cluster_center + radius_distance
         cluster_radius_on_image = solver.project_on_roof_camera(cluster_radius)
 
         cluster_center_on_image = solver.project_on_roof_camera(cluster_center)
-        distance = np.linalg.norm(cluster_radius_on_image-cluster_center_on_image)#compute the distance between the center and the radius in pixels
-        cv2.circle(blank_image, (int(cluster_center_on_image[0]), int(cluster_center_on_image[1])), int(distance), (14, 57, 43),annot.lw)
-        
-        if len(cluster_points)>=2:
-            cv2.putText(blank_image, 'Roda de Conversa', (int(cluster_center_on_image[0]+distance/2), int(cluster_center_on_image[1])), cv2.FONT_HERSHEY_SIMPLEX,0.6, (139, 0, 0), 1)
+        distance = np.linalg.norm(cluster_radius_on_image - cluster_center_on_image)
+
+        cv2.circle(blank_image, (int(cluster_center_on_image[0]), int(cluster_center_on_image[1])),
+                   int(distance), (14, 57, 43), annot.lw)
+
+        if len(cluster_points) >= 2:
+            cv2.putText(blank_image, 'Roda de Conversa',
+                        (int(cluster_center_on_image[0] + distance / 2), int(cluster_center_on_image[1])),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.6, (139, 0, 0), 1)
         else:
-            cv2.putText(blank_image, 'Pessoa', (int(cluster_center_on_image[0]+distance/2), int(cluster_center_on_image[1])), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (139, 0, 0), 1)
-    # Display the image
+            cv2.putText(blank_image, 'Pessoa', (int(cluster_center_on_image[0] + distance / 2),
+                                                int(cluster_center_on_image[1])), cv2.FONT_HERSHEY_SIMPLEX, 0.6,
+                        (139, 0, 0), 1)
+
+    # Display the annotated images
     cv2.imshow('output', img_orig)
-    #flip the image to be easier to understand (mirror image)
-    # blank_image = cv2.flip(blank_image, 1)
     cv2.imshow('blank_image', blank_image)
 
-    # print("FPS: ", 1.0 / (time.time() - start_time))
-    # print("ms ", (time.time() - start_time))
+    # Exit the loop if 'q' is pressed
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+
+# Release the video capture and close windows
 cap.release()
 cv2.destroyAllWindows()
-
